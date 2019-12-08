a02b401a7251624d0837ecbf7efd202a3055ba03
codingcyber <vivek@codingcyber.com>
2019-12-08 20:45:51 +0530

    5-2 CSRF Protection for Add Stock Page

--- add-stock.php
+++ add-stock.php
@@ -1,12 +1,49 @@
 <?php
+session_start();
 // CSRF Token Protection
 if(isset($_POST) & !empty($_POST)){
     print_r($_POST);
     // PHP Form Validations
     if(empty($_POST['stock'])){ $errors[] = "Stock Field is Required"; }else{
-        // chekc the symbol in unique with db query (select)
+        // chekc the symbol is unique with db query (select)
+    }
+
+    //3. Validate the CSRF Token (CSRF Token Validation & CSRF Token Time Validation)
+    // CSRF Token Validation
+    if(isset($_POST['csrf_token'])){
+        if($_POST['csrf_token'] === $_SESSION['csrf_token']){
+        }else{
+            $errors[] = "Problem with CSRF Token Verification";
+        }
+    }else{
+        $errors[] = "Problem with CSRF Token Validation";
+    }
+
+    // CSRF Token Time Validation
+    $max_time = 5; // time in seconds
+    if(isset($_SESSION['csrf_token_time'])){
+        // compare the time with maxtime
+        $token_time = $_SESSION['csrf_token_time'];
+        if(($token_time + $max_time) >= time()){ // nothing here
+        }else{
+            // display error message and unset the CSRF Tokens
+            $errors[] = "CSRF Token Expired";
+            unset($_SESSION['csrf_token']);
+            unset($_SESSION['csrf_token_time']);
+        }
+    }else{
+        // unset the CSRF Tokens
+        unset($_SESSION['csrf_token']);
+        unset($_SESSION['csrf_token_time']);
     }
 }
+//1. Create CSRF Token
+$token = md5(uniqid(rand(), TRUE));
+$_SESSION['csrf_token'] = $token;
+$_SESSION['csrf_token_time'] = time();
+
+//2. Add CSRF Token to Form
+
 include('includes/header.php');
 include('includes/navigation.php');
 ?>
@@ -38,7 +75,7 @@ include('includes/navigation.php');
                         <div class="col-lg-12">
                             <form role="form" method="post">
                                 <div class="col-lg-6">
-                                    <input type="hidden" name="csrf_token" value="<?php //echo $token; ?>">
+                                    <input type="hidden" name="csrf_token" value="<?php echo $token; ?>">
                                     <div class="form-group">
                                         <label>Stock Scrip</label>
                                         <input class="form-control" name="stock" placeholder="Enter Stock Scrip Name">
