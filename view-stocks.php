568a8857d2d4ad9cb43e90e27a574bcbee689f6a
codingcyber <vivek@codingcyber.com>
2019-12-09 13:55:24 +0530

    6-2 Calculating the Start Price, Date & Current Price, Date and Number of Days

--- view-stocks.php
+++ view-stocks.php
@@ -36,21 +36,44 @@ include('includes/navigation.php');
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php
-                                    $sql = "SELECT * FROM stocks";
-                                    $result = $db->prepare($sql);
-                                    $result->execute() or die(print_r($result->errorInfo(), true));
-                                    $stocks = $result->fetchAll(PDO::FETCH_ASSOC);
-                                    foreach ($stocks as $stock) {
-                                        // We can get the number of days by counting the number of rows in db
-                                ?>
+<?php
+    $sql = "SELECT * FROM stocks";
+    $result = $db->prepare($sql);
+    $result->execute() or die(print_r($result->errorInfo(), true));
+    $stocks = $result->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($stocks as $stock) {
+        // We can get the number of days by counting the number of rows in db
+        $dayssql = "SELECT * FROM stock_daily_values WHERE stockid=?";
+        $daysresult = $db->prepare($dayssql);
+        $daysres = $daysresult->execute(array($stock['id'])) or die(print_r($daysresult->errorInfo(), true));
+        $dayscount = $daysresult->rowCount();
+
+        // we should get the first & last records for Start & Current Price
+        $sql = "SELECT * FROM stock_daily_values WHERE stockid=? ORDER BY trade_date ASC LIMIT 1";
+        $result = $db->prepare($sql);
+        $result->execute(array($stock['id'])) or die(print_r($result->errorInfo(), true));
+        $stockstartvals = $result->fetch(PDO::FETCH_ASSOC);
+
+        $sql = "SELECT * FROM stock_daily_values WHERE stockid=? ORDER BY trade_date DESC LIMIT 1";
+        $result = $db->prepare($sql);
+        $result->execute(array($stock['id'])) or die(print_r($result->errorInfo(), true));
+        $stockcurrentvals = $result->fetch(PDO::FETCH_ASSOC);
+
+        // Select Sql queries for getting All time low & Highs
+
+?>
                                 <tr>
                                     <td><?php echo $stock['id']; ?></td>
-                                    <td><a href="view-stock.php?scrip=<?php echo $stock['symbol']; ?>"><?php echo $stock['symbol']; ?></a></td>
+                                    <td><a href="view-stock.php?scrip=<?php echo $stock['symbol']; ?>"><?php echo $stock['symbol']; ?></a><br><small><?php echo $stock['name']; ?></small>
+                                    </td>
                                     <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
+                                    <td><?php echo $dayscount; ?></td>
+                                    <td><?php echo round($stockstartvals['price_open'],2); ?>
+                                        <br><small><?php echo $stockstartvals['trade_date']; ?></small>
+                                    </td>
+                                    <td><?php echo round($stockcurrentvals['price_open'],2); ?>
+                                        <br><small><?php echo $stockcurrentvals['trade_date']; ?></small>
+                                    </td>
                                     <td>Mark</td>
                                     <td>Otto</td>
                                     <td><?php echo $stock['exchange']; ?></td>
