bc061ccd7c3a732da02704d95ac15db6f6ade849
codingcyber <vivek@codingcyber.com>
2019-12-14 13:36:41 +0530

    8-4 Display the Weekly & Monthly Values in Candle Stick Chart

--- chart.php
+++ chart.php
@@ -1,6 +1,24 @@
 <?php
 require_once('includes/connect.php');
-$sql = "SELECT * FROM stock_daily_values sdv JOIN stocks s ON sdv.stockid=s.id WHERE s.symbol=? ORDER BY trade_date ";
+
+if(isset($_GET['type']) & !empty($_GET['type'])){
+
+  switch ($_GET['type']) {
+    case 'weekly':
+      $table = "stock_weekly_values";
+      break;
+    case 'monthly':
+      $table = "stock_monthly_values";
+      break;
+    default:
+      $table = "stock_daily_values";
+      break;
+  }
+}else{
+  $table = "stock_daily_values";
+}
+
+$sql = "SELECT * FROM $table sdv JOIN stocks s ON sdv.stockid=s.id WHERE s.symbol=? ORDER BY trade_date ";
 if(isset($_GET['days']) & !empty($_GET['days'])){ $sql .= " DESC LIMIT {$_GET['days']}"; }else{ $sql .= " ASC";}
 $result = $db->prepare($sql);
 $res = $result->execute(array($_GET['scrip'])) or die(print_r($result->errorInfo(), true));
@@ -43,7 +61,8 @@ if(isset($_GET['days']) & !empty($_GET['days'])){
     </script>
   </head>
   <body>
+    <h2><?php echo $_GET['scrip']; ?> - <?php if(isset($_GET['type']) & !empty($_GET['type'])){ echo $_GET['type']; }else{ echo "Daily";} ?><?php if(isset($_GET['days']) & !empty($_GET['days'])){ echo " - " .$_GET['days'] . " Days"; } ?> Candle Stick Chart</h2>
     <div id="chart_div" style="width: 100%; height: 550px;"></div>
-    <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=30">30 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=60">60 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=90">90 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=180">180 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=360">360 Days</a>
+    <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=30">30 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=60">60 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=90">90 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=180">180 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=360">360 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&type=weekly">Weekly</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&type=monthly">Monthly</a>
   </body>
 </html>
