86028b2cea817599dbfc85a7db70895dab6bb16c
codingcyber <vivek@codingcyber.com>
2019-12-14 09:57:40 +0530

    7-2 Displaying the Stock Daily Values in Candle Stick Chart

--- chart.php
+++ chart.php
@@ -1,3 +1,15 @@
+<?php
+require_once('includes/connect.php');
+$sql = "SELECT * FROM stock_daily_values sdv JOIN stocks s ON sdv.stockid=s.id WHERE s.symbol=? ORDER BY trade_date ";
+if(isset($_GET['days']) & !empty($_GET['days'])){ $sql .= " DESC LIMIT {$_GET['days']}"; }else{ $sql .= " ASC";}
+$result = $db->prepare($sql);
+$res = $result->execute(array($_GET['scrip'])) or die(print_r($result->errorInfo(), true));
+$stockvals = $result->fetchAll(PDO::FETCH_ASSOC);
+// use array_reverst while $_GET['days'] is set
+if(isset($_GET['days']) & !empty($_GET['days'])){
+  $stockvals = array_reverse($stockvals);
+}
+?>
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -7,16 +19,21 @@
 
   function drawChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Mon', 20, 28, 38, 45],
-      ['Tue', 31, 38, 55, 66],
-      ['Wed', 50, 55, 77, 80],
-      ['Thu', 77, 77, 66, 50],
-      ['Fri', 68, 66, 22, 15]
+      <?php
+        foreach ($stockvals as $stockval) {
+      ?>
+      // Date, Low, Open, Close, High
+      ['<?php echo $stockval['trade_date']; ?>', <?php echo $stockval['price_low']; ?>, <?php echo $stockval['price_open']; ?>, <?php echo $stockval['price_close']; ?>, <?php echo $stockval['price_high']; ?>],
+      <?php } ?>
       // Treat first row as data as well.
     ], true);
 
     var options = {
-      legend:'none'
+      legend:'none',
+      candlestick: {
+            fallingColor: { strokeWidth: 0, fill: '#a52714', stroke: '#a52714' }, // red
+            risingColor: { strokeWidth: 0, fill: '#0f9d58', stroke: '#0f9d58' }   // green
+          }
     };
 
     var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
@@ -27,5 +44,6 @@
   </head>
   <body>
     <div id="chart_div" style="width: 100%; height: 550px;"></div>
+    <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=30">30 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=60">60 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=90">90 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=180">180 Days</a> | <a href="http://localhost/Stock-Market-Application/chart.php?scrip=<?php echo $_GET['scrip']; ?>&days=360">360 Days</a>
   </body>
 </html>
